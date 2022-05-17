362369f061f804dff1a206f7844286c9da5efe1b
Miguel Urena <urena_miguel82@outlook.com>
2022-05-16 19:56:47 -0700

    Update javascript, html, and css

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let input = document.getElementById("input");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("button clicked");
+    formValidation();
 });
 
 let formValidation = () => {
