64ea59f3a411136bb0ec6fd8f91da5d0c7e8e210
Miguel Urena <urena_miguel82@outlook.com>
2022-05-16 19:39:38 -0700

    Add form and input

---
+++ main.js
@@ -0,0 +1,15 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+});
+
+let formValidation = () => {
+    if() {
+        console.log("failure");
+    } else {
+
+    }
+}
